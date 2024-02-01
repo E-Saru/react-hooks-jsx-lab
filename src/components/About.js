949b8653a9beb4a2de0a770198b75bf8a3a52cbd
E.Saru <efjeniah.mwawughanga@student.moringaschool.com>
2024-02-01 14:40:53 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>n this lab, we will be rendering a few children components in our top-most component: App. The components are set up, but we need to update the JSX they return.</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
